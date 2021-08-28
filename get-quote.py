6c34764a7e621644a6598f9c956f8bf23a2a3fb0
LakshmiSowndarya Kota <lakshmisowndarya.kota@jda.com>
2021-08-28 19:27:06 +0530

    Added first quote

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,12 @@
 def main():
   print("Hello World")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+  f=open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  print(quotes)
+  
+  print(quotes[0])
 
 if __name__== "__main__":
   main()
